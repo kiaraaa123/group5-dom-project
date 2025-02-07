77595c8783d14f27e4ba9c04c3219868e381ad19
kariymam <59487728+kariymam@users.noreply.github.com>
2025-02-06 19:26:33 -0500

    Add API functions
    fetchData(): Is the FETCH request returns a json
    response from the api using axios. Needs to be parsed
    into a HTML row for the table
    
    submitData(e): is the POST request that takes an
    event 'e' parameter from the form submission and
    logs the POST response if successful.
    
    deleteData(): takes an event 'e' parameter because
    it will be attached to the Delete <button> on each
    row. Needs the ID for the data fetched and parsed
    into the row to include in the API url

---
+++ script.js
@@ -0,0 +1,73 @@
+const tbody = document.querySelector("#table tbody");
+const url = "https://bookstore-api-six.vercel.app/api/books";
+const cols = ["title", "author", "publisher"];
+
+const deleteData = (e) => {
+    const id = e.target.dataset.id // response.data["id"] should be saved to the record somehwere
+    
+    const bookURL = url + `/${id}`;
+    axios.delete(bookURL).then(() => {
+     console.log("Data deleted!");
+     
+    // HTML helper functions go here
+
+    });
+   };
+   
+   const fetchData = () => {
+    axios.get(url).then(
+     (response) => {
+      console.log(response);
+      
+      // HTML helper functions go here
+      
+     },
+     (error) => {
+      console.log(error.message);
+     }
+    );
+   };
+   
+   const submitData = (e) => {
+    e.preventDefault();
+    console.log(e.target)
+    const data = {};
+   
+    const getFormValues = (e) => {
+     const formValues = new FormData(e.target);
+     formValues.forEach((value, key) => (data[key] = value));
+    };
+   
+    const resetFormInputs = (e) => {
+     const inputs = e.target.querySelectorAll("input");
+     return inputs.forEach((el) => (el.value = ""));
+    };
+   
+    getFormValues(e);
+    resetFormInputs(e);
+   
+    axios
+     .post(url, data, {
+      headers: {
+       "content-type": "application/json"
+      }
+     })
+     .then((response) => {
+      console.log("POST Response:", response);
+      console.log("Data was successfully sent");
+
+      // HTML helper functions go here
+     
+     })
+     .catch((error) => {
+      if (error) {
+       console.log(error.message);
+      }
+     });
+   };
+   
+   // FETCH --------------------
+   fetchData();
+   
+   // POST --------------------
+   document.forms["book_form"].addEventListener("submit", submitData);
